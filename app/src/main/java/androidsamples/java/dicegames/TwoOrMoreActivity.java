ac51ed84a21eadf16eafb5f9151ca7a1098bf347
TheRandomizer7 <yash.yelmame@gmail.com>
2023-09-26 17:13:05 +0530

    Added Two or More UI screen

--- app/src/main/java/androidsamples/java/dicegames/TwoOrMoreActivity.java
+++ app/src/main/java/androidsamples/java/dicegames/TwoOrMoreActivity.java
@@ -9,6 +9,6 @@ public class TwoOrMoreActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_two_or_more);
+    setContentView(R.layout.activity_two_or_more_potrait);
   }
 }
